e9fdc1158977e495858628322e7debebf9a1bcae
kel-man <kyyafuso@gmail.com>
2019-09-30 15:00:53 -0700

    Everything

--- src/factoid.jsx
+++ src/factoid.jsx
@@ -3,7 +3,10 @@ import React, {Component} from 'react';
 export default props => (
     <div className='fact-well'>
         <h4 className='fact-title'>Cat Fact:</h4>
-        <p onClick={props.toggleFact} className='fact-content'>{props.text}</p>
-        <button onClick={props.getFacts} className='change-fact'>Change Cat Fact!</button>
+        <p className='fact-content'>{props.text}</p>
+        <div className='fact-buttons'>
+            <button onClick={props.getFacts} className='change-fact'>Change Cat Fact!</button>
+            <button onClick={props.toggleFact} className='toggle-fact'>Hide Cat Fact</button>
+        </div>
     </div>
 )
